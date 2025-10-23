12c7562e0d1de5b15faa45243e902527d7609025
vasilisCM <vpafiliaris@conceptmaniax.com>
2025-10-23 13:25:30 +0300

    testing gitkraken

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@
           The everyday meaningful gestures, rituals, acts of
           <b>self-care</b>. <br />
           The <b>#momoments</b> that add value to the wellbeing of my
-          body, mind and soul.
+          body, mind and
         </p>
       </div>
       <div class="circular-shape">
