04ac212331b4b22ece00ed3f0ae4bf0ab70d6cde
vasilisCM <vpafiliaris@conceptmaniax.com>
2025-11-24 12:26:49 +0200

    create basic page templates

---
+++ founder.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: Founder */ ?>
+<?php get_header(); ?>
+
+
+<main
+    data-barba="container"
+    data-barba-namespace="founder">
+    <!-- Hero  -->
+    <?php include 'components/hero.php'; ?>
+
+    <section class="basic">
+        <div class="boxed centered">
+            <h2>Basic Section</h2>
+        </div>
+    </section>
+
+    <section class="basic last-section">
+        <div class="boxed centered">
+            <h2>Last Section</h2>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
---
+++ supplements.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: Supplements */ ?>
+<?php get_header(); ?>
+
+
+<main
+    data-barba="container"
+    data-barba-namespace="supplements">
+    <!-- Hero  -->
+    <?php include 'components/hero.php'; ?>
+
+    <section class="basic">
+        <div class="boxed centered">
+            <h2>Basic Section</h2>
+        </div>
+    </section>
+
+    <section class="basic last-section">
+        <div class="boxed centered">
+            <h2>Last Section</h2>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
