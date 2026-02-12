cf38c4858b0e38b1d24564714edf9cc4844fbbba
vasilisCM <vpafiliaris@conceptmaniax.com>
2026-02-12 14:47:33 +0200

    dropdown menu as class + parallax image home

--- src/js/animations/moveUp.js
+++ src/js/animations/moveUp.js
@@ -1,34 +1,41 @@
 const moveUp = (triggerElement, arr, delay = 1, additionalAnimations = []) => {
     console.log("moveUp() called");
-    const tl = gsap.timeline();
-    tl.fromTo(
-      arr,
-      {
-        // y: 0,
-        opacity: 0,
-      },
-      {
-        // y: -0,
-        opacity: 1,
-        ease: "power4.out",
-        stagger: 0.8,
-        duration: 3,
-        delay: delay,
-        scrollTrigger: {
-          trigger: triggerElement,
-          start: "top center",
-          end: "bottom center",
-          scrub: true,
-          markers: true,
-        }
-      }
-    );
+    const mm = gsap.matchMedia();
   
-    additionalAnimations.forEach((animation) => {
-      tl.to(animation.target, animation.props, animation.position || "<");
+    mm.add("(min-width: 1025px)", () => {
+      const tl = gsap.timeline();
+      tl.fromTo(
+        arr,
+        {
+          y: 150,
+        },
+        {
+          y: -40,
+          ease: "power2.inOut",
+          stagger: 0.8,
+          duration: 10,
+          delay: delay,
+          scrollTrigger: {
+            trigger: triggerElement,
+            start: "top center",
+            end: "bottom center",
+            scrub: 3,
+          //   markers: true,
+          }
+        }
+      );
+    
+      additionalAnimations.forEach((animation) => {
+        tl.to(animation.target, animation.props, animation.position || "<");
+      });
+  
+      return () => {
+        // Cleanup: kill the timeline when the media query no longer matches
+        tl.kill();
+      };
     });
   
-    return tl;
+    return mm;
   };
   
   export default moveUp;
