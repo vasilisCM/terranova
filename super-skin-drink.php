5b6791b3c6993de707e239e7ebf8aad377506c8e
polinkar <pkaragiannopoulou@conceptmaniax.com>
2026-02-26 18:56:08 +0200

    serum super skin drink

--- super-skin-drink.php
+++ super-skin-drink.php
@@ -9,21 +9,23 @@
     <?php include 'components/hero-with-bg.php'; ?>
 
     <section class="basic intro text-center">
-        <div class="boxed-sm centered intro__container">
+        <div class="boxed-sm centered intro__container text">
             <?php the_content(); ?>
         </div>
 
-        <div class="basic boxed centered">
-            <div class="basic icon-list"> <?php if (have_rows('icon_list')): ?>
+        <div class="basic boxed-md centered serum-organic-list">
+            <div class="serum-organic-list__container"> <?php if (have_rows('icon_list')): ?>
                     <?php while (have_rows('icon_list')): the_row();
-                                                    $heading = get_sub_field('heading');
-                                                    $image = get_sub_field('image');
-                                                    $text = get_sub_field('text');
+                                                                $heading = get_sub_field('heading');
+                                                                $image = get_sub_field('image');
+                                                                $text = get_sub_field('text');
                     ?>
-                        <div class="icon-list__item">
-                            <div class="icon-list__img-container"><img src="<?php echo $image; ?>" alt=""></div>
-                            <div class="heading-xs uppercase letter-spacing-medium"><?php echo $heading; ?></div>
-                            <div class="text"><?php echo $text; ?></div>
+                        <div class="serum-organic-list__item">
+                            <div class="serum-organic-list__img-container"><img src="<?php echo $image; ?>" alt="" class="card-icon__img"></div>
+                            <div>
+                                <div class="text bold"><?php echo $heading; ?></div>
+                                <div class="text"><?php echo $text; ?></div>
+                            </div>
                         </div>
                     <?php endwhile; ?>
                 <?php endif; ?>
@@ -42,19 +44,19 @@
         $subheading_2 = $ingredients['subheading_2'];
         $description_2 = $ingredients['description_2'];
         ?>
-        <div class="boxed centered">
+        <div class="boxed-md centered">
             <div class="heading-m "><?php echo $heading; ?></div>
             <div class="text"><?php echo $text; ?></div>
         </div>
 
-        <div class="boxed centered basic flex">
+        <div class="boxed-md centered basic flex super-skin-drink-ingredients__list">
             <?php if (have_rows('super_skin_drink__ingredients')): while (have_rows('super_skin_drink__ingredients')): the_row();
                     if (have_rows('list')): while (have_rows('list')): the_row();
                             $image = get_sub_field('image');
                             $text = get_sub_field('text');
             ?>
                             <div class="super-skin-drink-ingredients__item">
-                                <div class="super-skin-drink-ingredients__img-container"><img src="<?php echo $image; ?>" alt=""></div>
+                                <div class="super-skin-drink-ingredients__img-container"><img src="<?php echo $image; ?>" alt="" class="card-icon__img"></div>
                                 <div class="text"><?php echo $text; ?></div>
                             </div>
             <?php endwhile;
@@ -63,12 +65,15 @@
             endif; ?>
         </div>
 
-        <div class="boxed centered basic">
-            <div class="super-skin-drink-ingredients__img-container">
+    </section>
+
+    <section class="first-section super-skin-drink-hows">
+        <div class="boxed-md centered light-gray-bg">
+            <div class="super-skin-drink-hows__img-container">
                 <img src="<?php echo $image_1; ?>" alt="">
             </div>
 
-            <div class="two-col-grid italic">
+            <div class="super-skin-drink-hows__text-container basic two-col-grid italic">
                 <div class="text-container">
                     <div class="heading-s"><?php echo $subheading_1; ?></div>
                     <div class="text"><?php echo $description_1; ?></div>
@@ -86,15 +91,15 @@
     $text = $banner['text'];
     $button = get_field('super_skin_cards_button');
     ?>
-    <section class="basic white super-skin-drink-banner" style="background-image: url('<?php echo $image; ?>');">
-        <div class="boxed centered">
-            <div class="heading-m"><?php echo $text; ?></div>
+    <section class="basic-section white super-skin-drink-banner" style="background-image: url('<?php echo $image; ?>');">
+        <div class="boxed-md centered">
+            <div class="super-skin-drink-banner__heading heading-m line-height-s"><?php echo $text; ?></div>
         </div>
     </section>
 
-    <section class="basic last-section super-skin-drink-cards">
-        <div class="boxed centered super-skin-drink-cards__container">
-            <div class="flex super-skin-drink-cards__cards">
+    <section class="last-section super-skin-drink-cards">
+        <div class="boxed-md centered super-skin-drink-cards__container">
+            <div class="super-skin-drink-cards__cards">
                 <?php if (have_rows('super_skin_drink__cards')): while (have_rows('super_skin_drink__cards')): the_row();
 
                         $image = get_sub_field('image');
@@ -103,13 +108,13 @@
                 ?>
                         <div class="super-skin-drink-cards__card">
                             <div class="super-skin-drink-cards__img-container"><img src="<?php echo $image; ?>" alt=""></div>
-                            <div class="super-skin-drink-cards__text-container accent-bg white">
-                                <div>
-                                    <span class="heading-xs uppercase letter-spacing-medium">Step </span>
-                                    <span class="heading-ms"><?php echo get_row_index(); ?></span>
+                            <div class="super-skin-drink-cards__text-container accent-bg white inline-padding">
+                                <div class="super-skin-drink-cards__card-step">
+                                    <div class="heading-xs uppercase letter-spacing-medium super-skin-drink-cards__card-step-label">Step </div>
+                                    <div class="heading-ms semibold super-skin-drink-cards__card-step-number super-skin-drink-cards__card-step-number--<?php echo get_row_index(); ?>"><?php echo get_row_index(); ?></div>
                                 </div>
-                                <div class="heading-m"><?php echo $heading; ?></div>
-                                <div class="text"><?php echo $text; ?></div>
+                                <div class="heading-m line-height-s semibold"><?php echo $heading; ?></div>
+                                <div class="text basic"><?php echo $text; ?></div>
                             </div>
                         </div>
                 <?php
@@ -118,13 +123,12 @@
             </div>
 
             <a href="<?php echo $button['link']; ?>">
-                <button class="button text-button">
+                <button class="button text-button centered">
                     <span class="text-button button__text"><?php echo $button['text_1']; ?> <?php echo $button['text_2']; ?></span>
                 </button>
             </a>
         </div>
     </section>
-
 
 </main>
 
