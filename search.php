8e3a9d835d825ad04ec604bedb5b0bacd56e5dfa
Polina <pkaragiannopoulou@conceptmaniax.com>
2026-03-09 14:53:57 +0200

    search results

--- search.php
+++ search.php
@@ -4,29 +4,29 @@ get_header();
 
 <main data-barba="container"
     data-barba-namespace="search">
-    <section class="first-section hero last-section">
-        <div class="boxed centered">
+    <section class="search-results first-section hero">
+        <div class="search-results__container boxed-sm centered">
             <div>
                 <?php if (have_posts()) : ?>
                     <h2>
-                        <?php echo __('Αποτελέσματα αναζήτησης για:', 'terranova'); ?> '<?php echo get_search_query(); ?>'
+                        <?php echo __('Search results for:', 'terranova'); ?> '<?php echo get_search_query(); ?>'
                     </h2>
                 <?php else : ?>
                     <h2>
-                        <?php echo __('Δε βρέθηκαν αποτελέσματα για:', 'terranova'); ?> '<?php echo get_search_query(); ?>'
+                        <?php echo __('No results for:', 'terranova'); ?> '<?php echo get_search_query(); ?>'
                     </h2>
                 <?php endif; ?>
             </div>
-            <div class="search__results text-l">
+            <div class="search-results__list text-l">
                 <?php if (have_posts()) : ?>
                     <?php while (have_posts()) : the_post(); ?>
-                        <a href="<?php the_permalink(); ?>" class="search__result">
+                        <a href="<?php the_permalink(); ?>" class="basic search-results__item">
                             <?php the_title(); ?>
                         </a>
                     <?php endwhile; ?>
                 <?php else : ?>
                     <p>
-                        <?php echo __('Καμία ανάρτηση δεν ταιριάζει με τα κριτήρια αναζήτησης.', 'terranova'); ?>
+                        <?php echo __('No posts match the search criteria.', 'terranova'); ?>
                     </p>
                 <?php endif; ?>
             </div>
