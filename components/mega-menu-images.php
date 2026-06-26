d48169b0ea94a05892f958f4324825ff6f4d6cbd
Polina <pkaragiannopoulou@conceptmaniax.com>
2026-06-26 16:04:28 +0300

    mega-menu-images position on mobile

--- components/mega-menu-images.php
+++ components/mega-menu-images.php
@@ -1,8 +1,9 @@
-<div class="mega-menu-images boxed text white lowercase hidden-mobile">
-    <?php
+<?php
     $mega_menu_banner_1 = get_field('mega_menu_banner_1', 'option');
     $mega_menu_banner_2 = get_field('mega_menu_banner_2', 'option');
-    ?>
+?>
+
+<div class="mega-menu-images boxed text white lowercase"> 
     <a href="<?php echo $mega_menu_banner_1['link']; ?>" class="white mega-menu-images__item">
         <img src="<?php echo $mega_menu_banner_1['image']; ?>">
         <div class="mega-menu-images__overlay"><?php echo $mega_menu_banner_1['text']; ?></div>
@@ -12,3 +13,14 @@
         <div class="mega-menu-images__overlay"><?php echo $mega_menu_banner_2['text']; ?></div>
     </a>
 </div>
+
+<!-- <div class="mega-menu-images-mob boxed text white lowercase hidden-desktop">
+    <a href="<?php echo $mega_menu_banner_1['link']; ?>" class="white mega-menu-images-mob__item">
+        <img src="<?php echo $mega_menu_banner_1['image']; ?>">
+        <div class="mega-menu-images-mob__overlay"><?php echo $mega_menu_banner_1['text']; ?></div>
+    </a>
+    <a href="<?php echo $mega_menu_banner_2['link']; ?>" class="white mega-menu-images-mob__item">
+        <img src="<?php echo $mega_menu_banner_2['image']; ?>" alt="">
+        <div class="mega-menu-images-mob__overlay"><?php echo $mega_menu_banner_2['text']; ?></div>
+    </a>
+</div> -->
